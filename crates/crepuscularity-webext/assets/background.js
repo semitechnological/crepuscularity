4b4f2f1b3e4e5e465ca9109121639530363ac6db
undivisible <undivisible@vk.com>
2026-05-18 16:45:56 +1000

    fix(webext): register mv3 worker safely

--- crates/crepuscularity-webext/assets/background.js
+++ crates/crepuscularity-webext/assets/background.js
@@ -1,22 +1,28 @@
+import init, * as runtimeModule from "../vendor/runtime.js";
+
 const api = globalThis.browser ?? globalThis.chrome;
-const runtimeModule = await import("../vendor/runtime.js");
-const wasmBytes = await fetch("../vendor/runtime_bg.wasm").then((response) => response.arrayBuffer());
-await runtimeModule.default({ module_or_path: wasmBytes });
+const runtimeUrl = api.runtime.getURL("vendor/runtime_bg.wasm");
+const runtimeReady = fetch(runtimeUrl)
+  .then((response) => response.arrayBuffer())
+  .then((wasmBytes) => init({ module_or_path: wasmBytes }))
+  .then(() => {
+    runtimeModule.background_main?.();
+    return runtimeModule;
+  });
 
 api.runtime?.onMessage?.addListener((message, _sender, sendResponse) => {
   if (typeof runtimeModule.handle_background_message !== "function") return false;
-  runtimeModule.handle_background_message(message)
+  runtimeReady
+    .then(() => runtimeModule.handle_background_message(message))
     .then((response) => sendResponse(response))
     .catch((error) => sendResponse({ ok: false, error: String(error) }));
   return true;
 });
 
 api.runtime?.onInstalled?.addListener(() => {
-  runtimeModule.settings_seed?.().catch(() => {});
+  runtimeReady.then(() => runtimeModule.settings_seed?.()).catch(() => {});
 });
 
 api.storage?.onChanged?.addListener((_changes, area) => {
-  if (area === "sync") runtimeModule.settings_seed?.().catch(() => {});
+  if (area === "sync") runtimeReady.then(() => runtimeModule.settings_seed?.()).catch(() => {});
 });
-
-runtimeModule.background_main?.();
